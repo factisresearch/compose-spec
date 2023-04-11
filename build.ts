7f76e22f2c4ad99dcffbb4c1fa2a6e3b35d68e84
Pascal Bachor <bachor@medilyse.org>
2023-04-11 16:11:45 +0200

    Use the only spec available

--- build.ts
+++ build.ts
@@ -6,28 +6,26 @@ import { join } from 'path'
 import * as fs from 'fs'
 
 const directory = 'lib'
-const schemaUrlPattern: string = 'https://raw.githubusercontent.com/docker/compose/master/compose/config/config_schema_v{version}.json'
-const schemaVersions: string[] = ['1', '2.0', '2.1', '2.2', '2.3', '2.4', '3.0', '3.1', '3.2', '3.3', '3.4', '3.5', '3.6', '3.7']
+const url: string = 'https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json'
 
 rimraf.sync(directory)
 fs.mkdirSync(directory)
 
-const promises: Promise<{ compile: string, version: string }>[] = []
-for (const version of schemaVersions) {
-  const url = schemaUrlPattern.replace('{version}', version)
-  promises.push(axios.get(url).then((response: AxiosResponse) => {
-    return { compilePromise: compile(response.data, `docker-compose-spec-v${version}.ts`), version }
-  }).then((compilePromiseAndVersion: { compilePromise: Promise<string>, version: string }) => {
-    return compilePromiseAndVersion.compilePromise.then((compile: string) => {
-      return { compile, version }
-    })
-  }).then((compileAndVersion: { compile: string, version: string }) => {
-    writeFileSync(join(directory, `docker-compose-spec-v${version}.ts`), compileAndVersion.compile)
-    return compileAndVersion
-  }))
-}
+const promises: Promise<{ compile: string }>[] = []
 
-Promise.all(promises).then((resolved: { compile: string, version: string }[]) => {
+promises.push(axios.get(url).then((response: AxiosResponse) => {
+  return { compilePromise: compile(response.data, `docker-compose-spec.ts`) }
+}).then((compilePromiseAndVersion: { compilePromise: Promise<string> }) => {
+  return compilePromiseAndVersion.compilePromise.then((compile: string) => {
+    return { compile }
+  })
+}).then((compileAndVersion: { compile: string }) => {
+  writeFileSync(join(directory, `docker-compose-spec.ts`), compileAndVersion.compile)
+  return compileAndVersion
+}))
+
+
+Promise.all(promises).then((resolved: { compile: string }[]) => {
   console.log(`${resolved.length} files written in ${directory}.`)
 }).catch((err) => {
   console.error(err)
