0abbd0d83c0a31d927629032f7bb02182d81990f
Pascal Bachor <bachor@medilyse.org>
2023-04-11 22:02:04 +0200

    Refactor

--- build.ts
+++ build.ts
@@ -1,41 +1,16 @@
 import axios, { AxiosResponse } from 'axios'
 import { compile } from 'json-schema-to-typescript'
-import * as rimraf from 'rimraf'
 import { writeFileSync } from 'fs'
-import { join } from 'path'
-import * as fs from 'fs'
 
-const directory = 'lib'
 const url: string = 'https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json'
+const out: string = 'index.ts'
 
-rimraf.sync(directory)
-fs.mkdirSync(directory)
-
-const promises: Promise<{ compile: string }>[] = []
-
-promises.push(axios.get(url).then((response: AxiosResponse) => {
-  return { compilePromise: compile(response.data, `docker-compose-spec.ts`) }
-}).then((compilePromiseAndVersion: { compilePromise: Promise<string> }) => {
-  return compilePromiseAndVersion.compilePromise.then((compile: string) => {
-    return { compile }
-  })
-}).then((compileAndVersion: { compile: string }) => {
-  writeFileSync(join(directory, `docker-compose-spec.ts`), compileAndVersion.compile)
-  return compileAndVersion
-}))
-
-
-Promise.all(promises).then((resolved: { compile: string }[]) => {
-  console.log(`${resolved.length} files written in ${directory}.`)
-}).catch((err) => {
-  console.error(err)
-  process.exit(-1)
+await axios.get(url).then((response: AxiosResponse) =>
+    compile(response.data, 'docker-compose-spec').then(
+        (compile: string) =>
+            writeFileSync(out, compile)
+    )
+).catch((err) => {
+    console.error(err)
+    process.exit(-1)
 })
-
-
-
-
-
-
-
-
