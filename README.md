9890cf6af2c0632f593f359b1bad26b204c10d82
Pascal Bachor <bachor@medilyse.org>
2023-04-29 14:36:25 +0200

    Fix using wrong spec. Refactor

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-Docker Compose Spec TypeScript
-===============================================
-
-[![npm](https://img.shields.io/npm/v/@gfi-centre-ouest/docker-compose-spec-typescript.svg)](https://www.npmjs.com/package/@gfi-centre-ouest/docker-compose-spec-typescript)
-
-Docker Compose configuration file schemas as TypeScript interfaces.
-
-Package generated from [JSONSchema files of official Docker Compose project](https://github.com/docker/compose/tree/master/compose/config),
-thanks to [bcherny/json-schema-to-typescript](https://github.com/bcherny/json-schema-to-typescript).
+This project has been forked from https://github.com/inetum-orleans/docker-compose-spec-typescript and heavily altered.
--- build.ts
+++ build.ts
@@ -1,16 +1,19 @@
 import axios, { AxiosResponse } from 'axios'
 import { compile } from 'json-schema-to-typescript'
 import { writeFileSync } from 'fs'
+import { serializeError } from 'serialize-error'
 
-const url: string = 'https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json'
+// the specification is not versioned explicitly by design
+// see also https://github.com/compose-spec/compose-spec/blob/master/04-version-and-name.md
+const url: string = 'https://raw.githubusercontent.com/compose-spec/compose-spec/853d1a9b2301c20b76b867a22b4ef0faef79362e/schema/compose-spec.json'
 const out: string = 'index.ts'
 
 await axios.get(url).then((response: AxiosResponse) =>
-    compile(response.data, 'docker-compose-spec').then(
+    compile(response.data, 'compose-spec').then(
         (compile: string) =>
             writeFileSync(out, compile)
     )
 ).catch((err) => {
-    console.error(err)
+    console.error(serializeError(err))
     process.exit(-1)
 })
